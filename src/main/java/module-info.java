82ad7c1fa1db1f7d61f9c07595a7174d9a8366eb
allem <alle.maini2003@gmail.com>
2024-08-23 20:50:37 +0200

    2024-08-23

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,3 @@
-//module com.example.unimanagement {
-//    requires javafx.controls;
-//    requires javafx.fxml;
-//
-//    opens com.example.unimanagement to javafx.fxml;
-//    exports com.example.unimanagement;
-//
-//}
 open module com.example.unimanagement {
         requires java.desktop;
         requires java.sql;
